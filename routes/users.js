84dc91509b88915194b0ad28ed0782e7e1f0e538
TheAnswer07 <moisedia7@gmail.com>
2022-10-05 22:29:42 -0700

    Function to delete user successfully created

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 //Updating user
 
 router.put("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id || req.user.isAdmin) {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
         if(req.body.password) {
             try{
                 const salt = await bcrypt.genSalt(10);
@@ -32,7 +32,22 @@ router.put("/:id", async (req, res) => {
     }
 });
 
+
 //Deleting user
+
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
+        
+        try{
+            const user = await User.deleteOne(req.params.id);
+            res.status(200).json("Account successfully deleted!")
+        } catch {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can only delete your own personal account!");
+    }
+});
 
 
 //Get a user
