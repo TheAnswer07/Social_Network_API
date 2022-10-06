e7c9e414776bcf68c312140d5ec0e09eeb71619c
TheAnswer07 <moisedia7@gmail.com>
2022-10-05 22:39:01 -0700

    Function get user successfully created

--- routes/users.js
+++ routes/users.js
@@ -19,12 +19,12 @@ router.put("/:id", async (req, res) => {
                 return res.status(500).json(err);
             }
         }
-        try{
+        try {
             const user = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
             });
             res.status(200).json("Account successfully updated!")
-        } catch {
+        } catch (err) {
             return res.status(500).json(err);
         }
     } else {
@@ -38,10 +38,10 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     if(req.body.userId === req.params.id || req.body.isAdmin) {
         
-        try{
-            const user = await User.deleteOne(req.params.id);
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
             res.status(200).json("Account successfully deleted!")
-        } catch {
+        } catch (err) {
             return res.status(500).json(err);
         }
     } else {
@@ -52,6 +52,15 @@ router.delete("/:id", async (req, res) => {
 
 //Get a user
 
+router.get("/:id", async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            const {password,updatedAt, ...other } = user._doc;
+            res.status(200).json(other);
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+});
 
 //Follow a user
 
