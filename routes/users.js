5f98cc8d4206944f01ba1ba6e076432ceecb1429
TheAnswer07 <moisedia7@gmail.com>
2022-10-05 22:54:31 -0700

    Following function successfully created

--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,9 @@ router.get("/", (req, res) => {
 //Updating user
 
 router.put("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id || req.body.isAdmin) {
-        if(req.body.password) {
-            try{
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
                 const salt = await bcrypt.genSalt(10);
                 req.body.password = await bcrypt.hash(req.body.password, salt);
             } catch (err) {
@@ -36,8 +36,8 @@ router.put("/:id", async (req, res) => {
 //Deleting user
 
 router.delete("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id || req.body.isAdmin) {
-        
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+
         try {
             const user = await User.findByIdAndDelete(req.params.id);
             res.status(200).json("Account successfully deleted!")
@@ -53,16 +53,36 @@ router.delete("/:id", async (req, res) => {
 //Get a user
 
 router.get("/:id", async (req, res) => {
-        try {
-            const user = await User.findById(req.params.id);
-            const {password,updatedAt, ...other } = user._doc;
-            res.status(200).json(other);
-        } catch (err) {
-            return res.status(500).json(err);
-        }
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
 });
 
 //Follow a user
+
+router.put("/:id/follow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.body.userId } });
+                res.status(200).json("User has been successfully followed!")
+            } else {
+                return res.status(403).json("You're already following this user!");
+            }
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can't follow yourself!");
+    }
+});
 
 
 //Unfollw a user
