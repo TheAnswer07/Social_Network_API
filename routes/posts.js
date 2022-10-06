907b766e6f540b8e7e0dd0597d909b6339d2a449
TheAnswer07 <moisedia7@gmail.com>
2022-10-06 00:18:57 -0700

    Function to get all posts from timeline successfully created

--- routes/posts.js
+++ routes/posts.js
@@ -19,7 +19,7 @@ router.put("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if (post.userId === req.body.userId) {
-            await post.updateOne({$set:req.body});
+            await post.updateOne({ $set: req.body });
             res.status(200).json("Post successfully updated!")
         } else {
             res.status(403).json("You can only update your own post!")
@@ -47,16 +47,16 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
-//Liking a post
+//Liking/Disliking a post
 
 router.put("/:id/like", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if (!post.likes.includes(req.body.userId)) {
-            await post.updateOne({ $push: {likes: req.body.userId} });
+            await post.updateOne({ $push: { likes: req.body.userId } });
             res.status(200).json("Post successfully liked!")
         } else {
-            await post.updateOne({ $pull: {likes: req.body.userId} })
+            await post.updateOne({ $pull: { likes: req.body.userId } })
             res.status(200).json("Post successfully disliked!")
         }
     } catch (err) {
@@ -66,9 +66,31 @@ router.put("/:id/like", async (req, res) => {
 
 //Getting a post
 
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 //Getting all posts from own account's timeline
 
-
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+            return Post.find({ userId: friendId });
+        })
+        );
+        res.json(userPosts.concat(...friendPosts));
+    } catch (err) {
+    res.status(500).json(err);
+}
+});
 
 
 module.exports = router;
