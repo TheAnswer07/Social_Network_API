ed92f8df99f7cba9e74fa2c2fb9ca73421468d4d
TheAnswer07 <moisedia7@gmail.com>
2022-10-05 22:58:50 -0700

    Unfollowing user function successfully created. User requests completed

--- routes/users.js
+++ routes/users.js
@@ -71,7 +71,7 @@ router.put("/:id/follow", async (req, res) => {
             const currentUser = await User.findById(req.body.userId);
             if (!user.followers.includes(req.body.userId)) {
                 await user.updateOne({ $push: { followers: req.body.userId } });
-                await currentUser.updateOne({ $push: { followings: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.params.id } });
                 res.status(200).json("User has been successfully followed!")
             } else {
                 return res.status(403).json("You're already following this user!");
@@ -86,5 +86,27 @@ router.put("/:id/follow", async (req, res) => {
 
 
 //Unfollw a user
+
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
+                res.status(200).json("User successfully unfollowed!")
+            } else {
+                return res.status(403).json("Unfollowed user!");
+            }
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can't follow yourself!");
+    }
+});
+
+
 
 module.exports = router;
